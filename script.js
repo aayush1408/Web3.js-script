f4869b30718a451342201a660990fe756222ce26
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-12 11:29:49 +0530

    updated

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ if (typeof web3 !== 'undefined') {
   // set the provider you want from Web3.providers
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
+
+//List of the ethereum accounts
 var accounts  = web3.eth.accounts;
-// // console.log(accounts);
-// var blockNumber = web3.eth.blockNumber;
-// console.log(blockNumber-1);
-// var block = web3.eth.getBlock(blockNumber);
-// console.log(block);
+console.log(accounts);
+
+//Getting the block number of latest block
+var blockNumber = web3.eth.blockNumber;
+console.log(blockNumber-1);
+
+//Getting blocks
+var block = web3.eth.getBlock(blockNumber);
+console.log(block);
+
+//Getting the 10 recent blocks
 for (var i=0; i < 10; i++) {
   console.log(web3.eth.getBlock(web3.eth.blockNumber + i));
-}node_modules
+}
